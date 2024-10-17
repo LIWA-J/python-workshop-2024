1d17384a3983e96038e2ebe014d664b2caef11ca
LIWA-J <lijiong.wang@ramboll.com>
2024-10-17 12:45:54 +0200

    Update Program.py

--- Program.py
+++ Program.py
@@ -6,7 +6,9 @@ df = pd.read_csv('./data/timesheet_data.csv')
  
 #rename columns in my dataframe
 df.rename(columns={'Original Entry Date':'Date', 'Quantity Reg':'Hours'}, inplace=True)
- 
+
+# saving the dataframe
+df.to_csv('timesheet_export.csv')
  
 print(df)
 
